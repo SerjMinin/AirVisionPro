18b672f3088a439f7456860c59275d42b934da5e
SerjMinin <serjm777@gmail.com>
2026-07-26 16:42:46 +0300

    Удалён пустой неиспользуемый chart.js

--- Web-app/v1.0/chart.js
+++
@@ -1,5 +0,0 @@
-/* 
- * Copyright (c) 2026 Минин Сергей Александрович.
- * Licensed under the GNU Affero General Public License v3.0.
- * See LICENSE file in the project root for full license information.
- */
