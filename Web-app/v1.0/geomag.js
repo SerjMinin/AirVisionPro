c2c0cd953526f6410cdd1bfc955eeac8a2b92d70
SerjMinin <serjm777@gmail.com>
2026-07-28 22:24:38 +0300

    Правка в Магнитных бурях

--- Web-app/v1.0/geomag.js
+++ Web-app/v1.0/geomag.js
@@ -1,4 +1,4 @@
-upabase/* 
+/* 
  * Copyright (c) 2026 Минин Сергей Александрович.
  * Licensed under the GNU Affero General Public License v3.0.
  * See LICENSE file in the project root for full license information.
